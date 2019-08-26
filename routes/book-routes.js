dcb8a85b26b29cc24ad861251be932f1d75dcff7
Muhamad Chairul Aziz <m.chairul669@gmail.com>
2019-08-26 19:37:21 +0700

    fix middleware in booking routes

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -5,13 +5,13 @@ const auth = require('../middleware/authenticate');
 
 const BookController = require('../controllers/book-controller');
 
-app.use(auth.authorize);
-app.use(auth.handleAuthError());
 app.group('/api/v1', router => {
+    router.use(auth.authorize);
+    router.use(auth.handleAuthError);
     router.get('/booking/all', BookController.show);
     router.get('/booking/detail/:id', BookController.showdetail);
     router.post('/booking/add', BookController.store);
     router.patch('/booking/:id', BookController.edit)
-})
+});
 
 module.exports = app;
